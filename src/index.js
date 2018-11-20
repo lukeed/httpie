c6620b8775cf2d23313030ddd7e5de2826f8b368
Luke Edwards <luke.edwards05@gmail.com>
2018-11-19 20:05:05 -0800

    import module from project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,47 @@
-//
+import { parse } from 'url';
+import { request } from 'https';
+import { globalAgent } from 'http';
+
+export function send(method, uri, opts={}) {
+	return new Promise((res, rej) => {
+		let out = '';
+		let headers = opts.headers || {};
+		let { protocol, hostname, port, path } = parse(uri);
+		let o = { protocol, path, method, hostname, port, headers };
+		if (protocol === 'http:') o.agent = globalAgent;
+
+		let req = request(o, r => {
+			r.setEncoding('utf8');
+
+			r.on('data', d => {
+				out += d;
+			});
+
+			r.on('end', () => {
+				let type = r.headers['content-type'];
+				if (type && type.includes('application/json')) {
+					out = JSON.parse(out);
+				}
+				r.data = out;
+				(r.statusCode >= 400 ? rej : res)(r);
+			});
+		});
+
+		req.on('error', rej);
+
+		if (opts.body) {
+			let str = JSON.stringify(opts.body);
+			req.setHeader('content-type', 'application/json');
+			req.setHeader('content-length', Buffer.byteLength(str));
+			req.write(str);
+		}
+
+		req.end();
+	});
+}
+
+export const get = send.bind(null, 'GET');
+export const post = send.bind(null, 'POST');
+export const patch = send.bind(null, 'PATCH');
+export const del = send.bind(null, 'DELETE');
+export const put = send.bind(null, 'PUT');

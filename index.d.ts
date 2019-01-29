2702036bf8800a18187de1547c9ba08b2fe42ef4
Leah <me@hrmny.sh>
2019-01-29 20:45:49 +0100

    feat: add typings

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+import {Url} from "url";
+import {IncomingMessage} from "http";
+
+export interface HttpieOptions {
+	body: any,
+	headers: {
+		[name: string]: string
+	}
+}
+
+export interface HttpieResponse<T> extends IncomingMessage {
+	data: string | T,
+}
+
+export function send<T>(method: string, uri: string | Url, opts: HttpieOptions): Promise<HttpieResponse<T>>;
+
+declare function method<T>(uri: string | Url, opts: HttpieOptions): Promise<HttpieResponse<T>>;
+export {
+	method as get,
+	method as post,
+	method as patch,
+	method as del,
+	method as put,
+};

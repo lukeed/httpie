f36fcf10ec372ff371eb328522b7f0783877a8ba
Leah <me@hrmny.sh>
2019-01-31 04:49:11 +0100

    feat: TypeScript definitions (#2)
    * feat: add typings
    
    * fix: declare & make `opts` optional
    
    * fix: add `redirect` option
    
    * fix: update HttpieResponse<T=any> interface

---
+++ index.d.ts
@@ -0,0 +1,26 @@
+import {Url} from "url";
+import {IncomingMessage} from "http";
+
+export interface HttpieOptions {
+	body: any,
+	headers: {
+		[name: string]: string
+	},
+	redirect: boolean,
+}
+
+export interface HttpieResponse<T = any> extends IncomingMessage {
+	data: T,
+}
+
+export declare function send<T>(method: string, uri: string | Url, opts?: HttpieOptions): Promise<HttpieResponse<T>>;
+
+declare function method<T>(uri: string | Url, opts?: HttpieOptions): Promise<HttpieResponse<T>>;
+
+export {
+	method as get,
+	method as post,
+	method as patch,
+	method as del,
+	method as put,
+};
